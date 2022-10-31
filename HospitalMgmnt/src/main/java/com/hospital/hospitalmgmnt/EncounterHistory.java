e771959afe15571f913193a43d653e0503c16987
rajputdi <rajput.di@northeastern.edu>
2022-10-30 21:24:38 -0400

    commit 10/30 --v15

--- HospitalMgmnt/src/main/java/com/hospital/hospitalmgmnt/EncounterHistory.java
+++ HospitalMgmnt/src/main/java/com/hospital/hospitalmgmnt/EncounterHistory.java
@@ -4,10 +4,21 @@
  */
 package com.hospital.hospitalmgmnt;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Divyesh Rajput
  */
 public class EncounterHistory {
+    ArrayList <Encounter> encounterHistoryList1;
+
+    public ArrayList<Encounter> getEncounterHistoryList1() {
+        return encounterHistoryList1;
+    }
+
+    public void setEncounterHistoryList1(ArrayList<Encounter> encounterHistoryList1) {
+        this.encounterHistoryList1 = encounterHistoryList1;
+    }
     
 }
